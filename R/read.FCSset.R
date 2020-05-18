8435003912abd5728e7339d3fde4dda22472672b
jgarces <jgarces@unav.es>
2020-05-18 13:26:18 +0200

    Correct events NULL behaviour

--- R/read.FCSset.R
+++ R/read.FCSset.R
@@ -22,12 +22,16 @@
 
 read.FCSset <- function(filelist = NULL, directory = getwd(), pattern = ".fcs$", 
                         events = "all", dataset = 1, num.threads = NULL){
-  if(events == "all") events <- NULL
   if(is.null(filelist)) print(filelist <- list.files(path = directory, pattern = pattern, full.names = T))
   
   if(Sys.info()[1] == "Windows") require(parallelsugar) #devtools::github_install('nathanvan/parallelsugar')
-  fcs <- ncdfFlow::as.flowSet(ncdfFlow::read.ncdfFlowSet(filelist, emptyValue = FALSE, transformation = FALSE, truncate_max_range = FALSE, 
-                                        which.lines = events, dataset = dataset, mc.cores = num.threads))
+  if(events == "all"){
+  	fcs <- ncdfFlow::as.flowSet(ncdfFlow::read.ncdfFlowSet(filelist, emptyValue = FALSE, transformation = FALSE, truncate_max_range = FALSE, 
+                                which.lines = NULL, dataset = dataset, mc.cores = num.threads))
+  	}else{
+	fcs <- ncdfFlow::as.flowSet(ncdfFlow::read.ncdfFlowSet(filelist, emptyValue = FALSE, transformation = FALSE, truncate_max_range = FALSE, 
+                                which.lines = events, dataset = dataset, mc.cores = num.threads))
+  	}
   
   return(fcs)
 }
