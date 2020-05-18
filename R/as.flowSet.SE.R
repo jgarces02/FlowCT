7b3d55363a68b19a7ea1e987a2ffdb49473154cd
jgarces <jgarces@unav.es>
2020-05-18 12:55:04 +0200

    Remove bad example and correct assay.i option

--- R/as.flowSet.SE.R
+++ R/as.flowSet.SE.R
@@ -7,16 +7,12 @@
 #' @keywords flowset generation
 #' @export
 #' @importFrom SummarizedExperiment colData
-#' @examples
-#' \dontrun{
-#'  sc_metadata <- scMetadata.fromFCS(fcs, metadata, add.exprs = T)
-#' }
 
 as.flowSet.SE <- function(fcs.SE, assay.i){
   df_flowset <- list()
   for(i in unique(fcs.SE$filename)){
     aux_fcsSE <- fcs.SE[,fcs.SE$filename == i]
-    df_flowset[[i]] <- premessa::as_flowFrame(t(assay(aux_fcsSE, i = "raw")))
+    df_flowset[[i]] <- premessa::as_flowFrame(t(assay(aux_fcsSE, i = assay.i)))
   }
   names(df_flowset) <- fcs.SE@metadata$input_fcs
   df_flowset <- methods::as(df_flowset, "flowSet")
